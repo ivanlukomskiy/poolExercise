10a157ec18b03ec8e92792c1e25fdc3dcb072fa2
ivanl <ilukomskiy@sbdagroup.com>
2018-07-06 16:52:11 +0300

    Initial commit

---
+++ src/main/java/Pool.java
@@ -0,0 +1,95 @@
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+/**
+ * Created by ivanl <ilukomskiy@sbdagroup.com> on 06.07.2018.
+ */
+public class Pool {
+
+    interface DataSource {
+        Connection getConnection() throws Exception;
+    }
+
+    interface Connection {
+        void close() throws Exception;
+
+        void doSomething() throws Exception;
+    }
+
+    static class DataSourcePool implements DataSource {
+
+        private final int limit;
+        private final DataSource originalDataSource;
+        private final int timeout;
+
+        private AtomicInteger connectionsCreated = new AtomicInteger(0);
+        private final BlockingQueue<Connection> freeConnections = new LinkedBlockingQueue<>();
+
+        DataSourcePool(int limit, int timeout, DataSource dataSource) {
+            this.limit = limit;
+            this.timeout = timeout;
+            originalDataSource = dataSource;
+        }
+
+        public Connection getConnection() throws Exception {
+            boolean canCreateNewConnection;
+
+            // Try to return free connection if present
+            Connection connection = freeConnections.poll();
+            if (connection != null) {
+                return new PooledConnection(connection, this);
+            }
+            canCreateNewConnection = connectionsCreated.getAndUpdate(i -> i < limit ? ++i : limit) < limit;
+
+            if (canCreateNewConnection) {
+                return new PooledConnection(originalDataSource.getConnection(), this);
+            } else {
+                // Wait for the next available free connection
+                Connection freeConnection = freeConnections.poll(timeout, MILLISECONDS);
+                if (freeConnection == null) {
+                    throw new TimeoutException("Timeout exceed");
+                }
+                return new PooledConnection(freeConnection, this);
+            }
+        }
+
+        void releaseConnection(Connection connectionOrigin) {
+            freeConnections.add(connectionOrigin);
+        }
+
+        int getConnectionsCreated() {
+            return connectionsCreated.get();
+        }
+    }
+
+    static class PooledConnection implements Connection {
+
+        private final Connection origin;
+        private final DataSourcePool pool;
+        private boolean closed = false;
+
+        PooledConnection(Connection origin, DataSourcePool pool) {
+            this.origin = origin;
+            this.pool = pool;
+        }
+
+        public void close() throws Exception {
+            if (!closed) {
+                closed = true;
+                pool.releaseConnection(origin);
+            }
+        }
+
+        public void doSomething() throws Exception {
+            if (closed) {
+                throw new IOException("Connection is closed");
+            }
+            origin.doSomething();
+        }
+    }
+}
---
+++ src/test/java/PoolTest.java
@@ -0,0 +1,112 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeoutException;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by ivanl <ilukomskiy@sbdagroup.com> on 06.07.2018.
+ */
+public class PoolTest {
+    private static final int POOL_LIMIT = 4;
+    private static final int POOL_TIMEOUT = 2000;
+
+    private Pool.DataSourcePool dataSource;
+
+    @Before
+    public void initConnectionsPool() throws Exception {
+        Pool.DataSource dataSourceOrigin = mock(Pool.DataSource.class);
+        when(dataSourceOrigin.getConnection()).thenAnswer(invocation -> mock(Pool.Connection.class));
+        dataSource = new Pool.DataSourcePool(POOL_LIMIT, POOL_TIMEOUT, dataSourceOrigin);
+    }
+
+    @Test
+    public void testGetConnection() throws Exception {
+        Pool.Connection connection = dataSource.getConnection();
+        assertNotNull(connection);
+    }
+
+    @Test
+    public void testDoSomethingWithConnection() throws Exception {
+        Pool.Connection connection = dataSource.getConnection();
+        connection.doSomething();
+    }
+
+    @Test(expected = IOException.class)
+    public void testCantDealWithClosedConnection() throws Exception {
+        Pool.Connection connection = dataSource.getConnection();
+        connection.close();
+        connection.doSomething();
+    }
+
+    @Test(expected = TimeoutException.class)
+    public void testExceedConnectionsLimit() throws Exception {
+        for (int i = 0; i < POOL_LIMIT + 1; i++) {
+            dataSource.getConnection();
+        }
+    }
+
+    @Test
+    public void testWaitForFreeConnection() throws Exception {
+        Pool.Connection connection = dataSource.getConnection();
+        // Close connection after a second
+        new Thread(() -> {
+            try {
+                Thread.sleep(POOL_TIMEOUT / 2);
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        for (int i = 0; i < POOL_LIMIT; i++) {
+            dataSource.getConnection();
+        }
+    }
+
+    @Test
+    public void testConnectionsReused() throws Exception {
+        Pool.Connection connection = dataSource.getConnection();
+        connection.doSomething();
+        connection.close();
+        dataSource.getConnection();
+        assertEquals(dataSource.getConnectionsCreated(), 1);
+    }
+
+    @Test
+    public void testConcurrentAccess() throws Exception {
+        CyclicBarrier barrier = new CyclicBarrier(POOL_LIMIT + 1);
+
+        Runnable dbTask = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // Await for other threads to initialize
+                    barrier.await();
+
+                    for (int i = 0; i < 1000; i++) {
+                        Pool.Connection connection = dataSource.getConnection();
+                        connection.doSomething();
+                        connection.close();
+                    }
+                    barrier.await(1, SECONDS);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        for (int i = 0; i < POOL_LIMIT; i++) {
+            new Thread(dbTask).start();
+        }
+        barrier.await(1, SECONDS); // Wait for the threads to get ready
+        barrier.await(1, SECONDS); // Wait for the threads to finish
+    }
+}
